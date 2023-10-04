7fcbf2547eac92f75f322bb5b80ac2e3bd1cedfe
adrianag57 <adrianag57@educastur.es>
2023-10-04 09:43:48 +0200

    cambios

---
+++ js/ejercicio5.js
@@ -0,0 +1,14 @@
+const size = 50;
+const defaultValue = 0;
+let arrayManual = new Array(size);
+
+for (let i = 0; i < arrayManual.length; i++;) {
+
+    arrayManual[i] = defaultvalue;
+}
+
+console.log(arrayManual);
+
+let arrPro = new Array(size);
+arrPro.fill(defaultValue);
+console.log(arrPro);
